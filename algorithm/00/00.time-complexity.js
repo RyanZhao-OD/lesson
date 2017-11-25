ad3c0af11273ef86ea5e2277cbe71e1aed6a3bf8
ryanzhao1993 <ryanzhao1993@qq.com>
2017-11-25 18:29:21 +0800

    commit

---
+++ algorithm/00/00.time-complexity.js
@@ -0,0 +1,13 @@
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+const printArray = arr => {
+    for (let i = 0, length = arr.length; i < length; i++) {
+        for (let j = 0, lengthIn = arr[i].length; j < lengthIn; j++ ) {
+            console.log(arr[i][j]);
+        }
+    }
+};
+printArray(arr);
